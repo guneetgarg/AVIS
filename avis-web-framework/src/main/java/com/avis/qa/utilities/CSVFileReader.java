52b62b4ca451fab44007858e6f476d479ec6dd6d
Choudhary Ankur <abc@gmail.com>
2023-02-02 23:02:18 +0530

    updated filepath to static

--- avis-web-framework/src/main/java/com/avis/qa/utilities/CSVFileReader.java
+++ avis-web-framework/src/main/java/com/avis/qa/utilities/CSVFileReader.java
@@ -1,5 +1,9 @@
 package com.avis.qa.utilities;
 
+import static com.avis.qa.core.Configuration.BRAND;
+import static com.avis.qa.core.Configuration.DOMAIN;
+
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
@@ -18,20 +22,41 @@ import com.opencsv.exceptions.CsvException;
 public class CSVFileReader {
 	static String filepath = null;
 	
+	
+	/**
+	 * loads the data parameter file path
+	 */
+	public static void loadDataParameter() {
+
+		if (BRAND != null && DOMAIN != null) {
+//			filepath = "./testdata/" + BRAND + "_DataParameter" + "_" + DOMAIN + ".csv";
+			filepath = "./testdata/Paylesscar_DataParameter_US.csv";
+			System.out.println("CREATED FILE PATH = " + filepath);
+		} else {
+			System.out.println("NULL RESULTS");
+			System.exit(0);
+		}
+
+	}
+	
 	@DataProvider(name = "dataAsMap")
 	public static Map<String, String>[][] readDataLineBymethodName(Method method) throws IOException, CsvException {
+		loadDataParameter();
 		int rowCount;
 		System.out.println("======= "+System.getProperty("user.dir"));
 //		System.out.println(System.getProperty("user.dir")+"\\testData\\Paylesscar_DataParameter_US.csv");
 //		String filepath = System.getProperty("user.dir")+"/testData/Paylesscar_DataParameter_US.csv";
-		String filepath = "./testData/Paylesscar_DataParameter_US.csv";
+//		String filepath = "./testData/Paylesscar_DataParameter_US.csv";
 		System.out.println("=====filePath");
 //		CSVFileReader csvFileReader = new CSVFileReader();
 		
-		System.out.println("###############3 "+filepath);
+		System.out.println("############### "+filepath);
 		// Create an object of filereader
 		// class with CSV file as a parameter.
-		InputStreamReader filereader = new InputStreamReader(new FileInputStream(filepath), StandardCharsets.UTF_8);
+//		InputStreamReader filereader = new InputStreamReader(new FileInputStream(filepath), StandardCharsets.UTF_8);
+		
+		
+		BufferedReader filereader = new BufferedReader(new FileReader(filepath));
 //		FileReader filereader = new FileReader(filepath);
 			System.out.println("FileReader");
 		// create csvReader object passing
--- avis-web-framework/src/main/java/com/avis/qa/utilities/CSVUtils.java
+++ avis-web-framework/src/main/java/com/avis/qa/utilities/CSVUtils.java
@@ -51,7 +51,6 @@ public class CSVUtils {
 		try {
 			Thread.sleep(1000);			
 			br = new BufferedReader(new FileReader(filepath));
-//			InputStreamReader ir = new InputStreamReader(new FileInputStream(filepath), StandardCharsets.UTF_8);
 			while ((line = br.readLine()) != null) {
 				value = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
 				if (value[0].equals(methodName)) {
