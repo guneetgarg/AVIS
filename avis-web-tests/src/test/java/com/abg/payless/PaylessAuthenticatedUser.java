381e1e89fad9912875e481662869b43488392026
Choudhary Ankur <abc@gmail.com>
2023-02-08 13:19:12 +0530

    payless fixes

---
+++ avis-web-tests/src/test/java/com/abg/payless/PaylessAuthenticatedUser.java
@@ -0,0 +1,17 @@
+package com.abg.payless;
+
+import org.testng.annotations.Test;
+
+import com.avis.qa.core.TestBase;
+
+public class PaylessAuthenticatedUser extends TestBase{
+	
+	
+	@Test
+	public void Paylesscar_RES_Inbound_IATA_M_type_PayLater_US(){
+		
+		
+		
+	}
+
+}
